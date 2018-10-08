d467e7ee4fa4bb2fbfab616a262b58049b7258f6
Onii <Onii@GLaDOS>
2018-10-08 23:03:49 +0200

    Ej10

--- EjerciciosIF/src/ejerciciosif/Ejercicio10.java
+++ EjerciciosIF/src/ejerciciosif/Ejercicio10.java
@@ -17,6 +17,60 @@ private static Scanner sc = new Scanner(System.in);
 		int mes = sc.nextInt();
 		System.out.println("Año:");
 		int anyo = sc.nextInt();
+		
+		if (mes >= 1 && mes <= 12) {
+			switch (mes) {
+			case 2:
+				if (dia >= 1 && dia <= 28)
+					dia = dia + 1; 
+				if (dia > 28) {
+					dia=1;
+					mes = mes+1;
+				}
+				if (mes >12) {
+					mes=1;
+					anyo = anyo+1;
+				}
+				break;
+			case 1:
+			case 3:
+			case 5:
+			case 7:
+			case 8:
+			case 10:
+			case 12:
+				if (dia >= 1 && dia <= 31)
+					dia = dia + 1; 
+				if (dia > 31) {
+					dia=1;
+					mes = mes+1;
+				}
+				if (mes >12) {
+					mes=1;
+					anyo = anyo+1;
+				}
+				break;
+			case 4:
+			case 6:
+			case 9:
+			case 11:
+				if (dia >= 1 && dia <= 30)
+					dia = dia + 1; 
+				if (dia > 30) {
+					dia=1;
+					mes = mes+1;
+				}
+				if (mes >12) {
+					mes=1;
+					anyo = anyo+1;
+				}
+				break;
+			default:
+				break;
+			}
+		
+		System.out.println(dia+"-"+mes+"-"+anyo);
 			
+		}
 	}
 }
